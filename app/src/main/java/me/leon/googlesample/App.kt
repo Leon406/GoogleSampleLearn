5489abc7bf39a799332a58f0d8141753f00e589c
Leon <shi406406@qq.com>
2019-01-09 19:57:42 +0800

    Initial commit

---
+++ app/src/main/java/me/leon/googlesample/App.kt
@@ -0,0 +1,17 @@
+package me.leon.googlesample
+
+import android.app.Application
+
+/**
+ * @author : Leon Shih
+ * @time : 2019/1/9 0009 15:09
+ * @e-mail : deadogone@gmail.com    :
+ * @desc :
+ */
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
